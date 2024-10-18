45cb133c4d98ab9bb4fe45f08845057ef9bf0c21
Bunkelic <157381826+Bunkelic@users.noreply.github.com>
2024-10-18 17:44:28 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,126 +1,90 @@
-// Primer 1
-let niz1 = [];
-for (let i = 0; i < 5; i++) {
-    niz1.push(i);
-}
-console.log(niz1); // [0, 1, 2, 3, 4]
+let firstName = "Andrej"; 
+let lastName = 'Buncic'; 
+let dugiStringUViseRedova = `Lista korisnika:
+* Korisnik1
+* Korisnik2
+* Korisnik3
+* Korisnik4`;
+console.log(dugiStringUViseRedova);
 
-// Primer 2
-let niz2 = [];
-for (let i = 0; i < 10; i += 2) {
-    niz2.push(i);
-}
-console.log(niz2); // [0, 2, 4, 6, 8]
+let firstChar = firstName.charAt(0);
+console.log(firstChar);
+console.log(firstName.length);
 
-// Primer 3
-let niz3 = [];
-for (let i = 10; i > 0; i -= 2) {
-    niz3.push(i);
-}
-console.log(niz3); // [10, 8, 6, 4, 2]
+let lastChar = firstName[firstName.length - 1]; 
+console.log(lastChar);
+console.log(firstName[15]); 
+console.log(firstName.charAt(15));
 
-// Primer 4
-let niz4 = [10, 9, 8, 7, 6];
-for (let i = 0; i < niz4.length; i++) {
-    console.log(niz4[i]); // 10, 9, 8, 7, 6
+for (let char of firstName) {
+    console.log(char);
 }
 
-// Primer 5
-let niz5 = [
-    [1, 2, 3], ['Petar', 'Petrovic'], ['Novi Sad', 42]
-];
-for (let i = 0; i < niz5.length; i++) {
-    for (let j = 0; j < niz5[i].length; j++) {
-        console.log(niz5[i][j]); // 1, 2, 3, Petar, Petrovic, Novi Sad, 42
-    }
+firstName[0] = 'd'; 
+console.log(firstName[0]);
+
+console.log(firstName.toLowerCase()); 
+console.log(firstName.toUpperCase());
+console.log(firstName[0].toLowerCase()); 
+console.log(firstName[firstName.length - 1].toUpperCase());
+
+let recenica = 'Danas je lep dan';
+console.log(recenica.indexOf("Danas")); 
+console.log(recenica.indexOf('an'));
+console.log(recenica.indexOf('an', 4)); 
+console.log(recenica.indexOf('noc')); 
+
+let target = 'an';
+let position = 0;
+let counter = 0;
+
+while (true) {
+    let pronadjenaPozicija = recenica.indexOf(target, position);
+    if (pronadjenaPozicija == -1) break;
+    position = pronadjenaPozicija + 1; 
+    counter++;
 }
 
-// Primer 6
-let ourArray = [];
-let i = 0;
-while (i < 5) {
-    ourArray.push(i);
-    i++;
-}
-console.log(ourArray); // [0, 1, 2, 3, 4]
+console.log(`Pronadjen substring 'an' u recenici 'Danas je lep dan' ${counter} puta`);
 
-// Primer 7
-let lastName = prompt('Kako se prezivas?');
-console.log(lastName);
+let recenica2 = 'Danas je suncan dan';
+target = 'an';
+position = -1;
+counter = 0;
 
-// Primer 8 (Switch)
-let job = prompt('Čime se Petar bavi ?');
-switch (job) {
-    case 'programer':
-        console.log('Petar je programer.');
-        break;
-    case 'nastavnik':
-        console.log('Petar je nastavnik.');
-        break;
-    case 'dizajner':
-        console.log('Petar je dizajner.');
-        break;
-    case 'lekar':
-        console.log('Petar je lekar.');
-        break;
-    default:
-        console.log('Petar je,...hm....stvarno nisam sigurna');
+while ((position = recenica2.indexOf(target, position + 1)) != -1) {
+    counter++;
+    console.log(`Pronadjen substring 'an' u recenici 'Danas je suncan dan' na ${position} poziciji`);
 }
 
-// Primer 9 (if...else)
-let brojGodinaPera = 15;
-let brojGodinaMika = 14;
-let brojGodinaLaza = 13;
-let sklekoviPera = 12;
-let sklekoviMika = 13;
-let sklekoviLaza = 11;
-let bodoviPera = brojGodinaPera + 5 * sklekoviPera;
-let bodoviMika = brojGodinaMika + 5 * sklekoviMika;
-let bodoviLaza = brojGodinaLaza + 5 * sklekoviLaza;
-
-if (bodoviPera > bodoviMika && bodoviPera > bodoviLaza) {
-    console.log('Pera je pobednik sa ukupno ' + bodoviPera + ' bodova!');
-} else if (bodoviMika > bodoviPera && bodoviMika > bodoviLaza) {
-    console.log('Mika je pobednik sa ukupno ' + bodoviMika + ' bodova!');
-} else if (bodoviLaza > bodoviPera && bodoviLaza > bodoviMika) {
-    console.log('Laza je pobednik sa ukupno ' + bodoviLaza + ' bodova!');
-} else {
-    console.log('Bravo za sve!');
+console.log(`Substring 'an' u recenici 'Danas je suncan dan' pronadjen ${counter} puta`);
+if (recenica2.indexOf('suncan') != -1) {
+    console.log('Substring je pronadjen!');
 }
 
-// Primer 10 (Pristupanje elementima niza)
-let names = ['Mika', 'Pera', 'Laza'];
-let years = new Array(2004, 2005, 2006);
-console.log(years); // [2004, 2005, 2006]
-console.log(years[0]); // 2004
-console.log(years[1]); // 2005
-console.log(years[2]); // 2006
+console.log(recenica2.lastIndexOf('suncan'));
 
-let petar = ['Petar', 'Petrovic'];
-console.log(petar); // ['Petar', 'Petrovic']
-petar.push(2005);
-console.log(petar); // ['Petar', 'Petrovic', 2005]
+console.log(recenica2.includes('Danas'));
+console.log(recenica2.startsWith('Danas'));
+console.log(recenica2.endsWith('suncan'));
+console.log(recenica2.endsWith('dan '));
 
-// Primer 11
-let years1 = [2005, 2011, 2014];
-let ages = [];
-let seniorDeveloper = [];
+let nekiString = "Dobar dan";
+console.log(nekiString.slice(0, 5)); 
+console.log(nekiString.slice(0, 1)); 
+console.log(nekiString.slice(1)); 
+console.log(nekiString.slice(-5, -2)); 
 
-function printSeniorDeveloper(years) {
-    for (let i = 0; i < years.length; i++) {
-        ages[i] = 2017 - years[i];
-    }
-    for (let i = 0; i < ages.length; i++) {
-        if (ages[i] >= 5) {
-            console.log('Person ' + (i + 1) + ' is a senior programmer with ' + ages[i] + ' years of experience');
-            seniorDeveloper[i] = true;
-        } else {
-            console.log('Person ' + (i + 1) + ' is not a senior programmer with ' + ages[i] + ' years of experience');
-            seniorDeveloper[i] = false;
-        }
-    }
-    return seniorDeveloper;
-}
+console.log(nekiString.substring(0, 5)); 
+console.log(nekiString.substring(0, 1)); 
+console.log(nekiString.substring(5, 0)); 
+console.log(nekiString.substring(1, 0)); 
 
-let isSenior = printSeniorDeveloper(years1);
-console.log(isSenior); // [true, true, false]
+console.log(nekiString.substr(0, 5)); 
+console.log(nekiString.substr(6, 2)); 
+console.log(nekiString.substr(-5, 2)); 
+
+let strForTrim = "   Neki string   ";
+console.log(strForTrim); 
+console.log(strForTrim.trim());
+
